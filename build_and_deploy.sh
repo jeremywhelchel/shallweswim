4e493b3f77329ed56d13aaf7dd62700bd1d15c83
jeremy.whelchel <jeremy.whelchel@gmail.com>
2023-02-12 19:14:04 -0500

    artifact registry

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,7 +2,7 @@
 
 set -e;
 
-IMAGE_TAG="gcr.io/shallweswim/shallweswim"
+IMAGE_TAG="us-east4-docker.pkg.dev/shallweswim/gcr/shallweswim"
 
 gcloud builds submit --tag ${IMAGE_TAG}
 gcloud run deploy --image ${IMAGE_TAG} shallweswim

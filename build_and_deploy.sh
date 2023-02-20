1960227394bb75b58a41fcaa38b430e37e165b3d
jeremy.whelchel <jeremy@whelchel.me>
2023-02-20 10:59:06 -0500

    cloud build config

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,8 +2,5 @@
 
 set -e;
 
-IMAGE_TAG="us-east4-docker.pkg.dev/shallweswim/gcr/shallweswim"
-
 gcloud config configurations activate shallweswim
-gcloud builds submit --tag ${IMAGE_TAG}
-gcloud run deploy --image ${IMAGE_TAG} shallweswim
+gcloud builds submit --config cloudbuild.yaml

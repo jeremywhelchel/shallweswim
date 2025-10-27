24d9f49b8d17d66d348c511c203879a20d6b059d
jeremy.whelchel <jeremy@whelchel.me>
2025-10-27 09:07:01 -0400

    disable boston. source broken

--- shallweswim/config.py
+++ shallweswim/config.py
@@ -641,6 +641,7 @@ _CONFIG_LIST = [
         description="Austin, TX open water swimming conditions",
     ),
     LocationConfig(
+        enabled=False,
         code="bos",
         name="Boston",
         swim_location="L Street Beach",
